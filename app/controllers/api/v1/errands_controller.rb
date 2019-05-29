eebcf804163f2751bd503e7762f16ad6095f3745
github username <github email address>
2019-05-29 14:42:41 -0600

    minor tweaks

--- app/controllers/api/v1/errands_controller.rb
+++ app/controllers/api/v1/errands_controller.rb
@@ -1,2 +1,33 @@
-class ErrandsController < ApplicationController
+module Api
+  module V1
+    class ErrandsController < ApplicationController
+      before_action :find_errand, only: [:update, :destroy]
+
+      def index
+        render json: Errand.all
+      end
+
+      def create
+        @errand = Errand.create(errand_params)
+      end
+
+      def update
+        @errand.update(errand_params)
+      end
+
+      def destroy
+        @errand.destroy
+      end
+
+      private
+
+      def find_errand
+        @errand = Errand.find(params[:id])
+      end
+
+      def errand_params
+        params.require(:errand).permit(:house_id, :name, :description)
+      end
+    end
+  end
 end
